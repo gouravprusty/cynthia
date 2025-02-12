5f20c2e4e5a51e6f52b8db05cad9a794ac41c0a0
Gourav Prusty <gouravprusty1432@gmail.com>
2025-02-12 13:30:46 +0530

    First page animation

--- js/script.js
+++ js/script.js
@@ -8,6 +8,37 @@ setTimeout(() => {
     loader.style.top = "-100%";
 }, 2000);
 
+function firstPageAnim(){
+    var tl = gsap.timeline();
+    tl.from("nav", {
+        y: '-100%',
+        opacity: 0,
+        delay: 1.8,
+        duration: 2,
+        ease: Expo.easeInOut
+    })
+    .to(".hero_head span", {
+        y: '0',
+        duration: 3,
+        delay: -2,
+        stagger: 0.2,
+        ease: Expo.easeInOut
+    })
+    .to(".available p .anim_el", {
+        y: '0',
+        duration: 3,
+        delay: -3,
+        stagger: 0.2,
+        ease: Expo.easeInOut
+    })
+    .to(".hero .links", {
+        opacity: 1,
+        duration: 2,
+        delay: -2,
+    })
+
+};
+
 document.addEventListener('DOMContentLoaded', function () {
     new Splide('.splide', {
         type   : 'loop',
@@ -22,14 +53,29 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function cursorFollower(){
+    let cursor = document.querySelector(".cursor");
     window.addEventListener("mouseover", () => {
-        document.querySelector(".cursor").style.opacity = "1";
+        cursor.style.opacity = "1";
     })
     window.addEventListener("mousemove", function(dets){
-        document.querySelector(".cursor").style.transform = `translate(${dets.clientX-8}px, ${dets.clientY-8}px)`;
+        const cursorSize = cursor.offsetWidth / 1.5;
+        cursor.style.transform = `translate(${dets.clientX-cursorSize}px, ${dets.clientY-cursorSize}px)`;
+        // cursor.style.transform = `translate(${dets.clientX-8}px, ${dets.clientY-8}px)`;
     })
     window.addEventListener("mouseout", () => {
-        document.querySelector(".cursor").style.opacity = "0";
+        cursor.style.opacity = "0";
+    })
+    let larger = document.querySelectorAll(".large_cursor");
+    larger.forEach((el) => {
+        el.addEventListener("mouseover", () => {
+            cursor.style.width = "30px";
+            cursor.style.height = "30px";
+        });
+        el.addEventListener("mouseout", () => {
+            cursor.style.width = "12px";
+            cursor.style.height = "12px";
+        });
     })
 }
 cursorFollower();
+firstPageAnim();
